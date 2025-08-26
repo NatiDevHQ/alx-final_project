40857fe5cde996256376a1ead2c731d0266f2ba9
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-26 18:47:28 -0400

    add Django REST Framework and token authentication to INSTALLED_APPS

--- task_management/settings.py
+++ task_management/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+      # Third-party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
 ]
 
 MIDDLEWARE = [
