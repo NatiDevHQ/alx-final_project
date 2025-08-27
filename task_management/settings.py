d4b29a4efa49358758374520006df544f0f99408
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-27 18:27:58 -0400

    add configure REST_FRAMEWORK authentication and permissions

--- task_management/settings.py
+++ task_management/settings.py
@@ -136,3 +136,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
