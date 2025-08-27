f394f106601cba70c655a2afd86321abd2e105aa
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-27 19:25:33 -0400

    custom user forms and admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ["id", "username", "email", "name", "is_staff", "is_active"]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
