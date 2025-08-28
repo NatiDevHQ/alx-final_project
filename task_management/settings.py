872dfbbd1eb1cec4d43db9bf3ebfdd85ed937ef8
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-28 17:08:17 -0400

    add CustomUser model with extra fields update the Rest

--- task_management/settings.py
+++ task_management/settings.py
@@ -46,9 +46,13 @@ INSTALLED_APPS = [
     'tasks',
     #debug tool
      "debug_toolbar",
+     "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",  
+    "django.middleware.common.CommonMiddleware",
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,13 +145,20 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
-    "DEFAULT_PERMISSION_CLASSES": [
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
         "rest_framework.permissions.AllowAny",
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
 
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
+CORS_ALLOW_ALL_ORIGINS = True
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    # Add custom fields here if needed
+    phone_number = models.CharField(_('Phone Number'), max_length=15, blank=True)
+    profile_picture = models.ImageField(_('Profile Picture'), upload_to='profiles/', blank=True)
+    date_of_birth = models.DateField(_('Date of Birth'), null=True, blank=True)
+    
+    # Add any additional fields you need
+    
+    def __str__(self):
+        return self.email or self.username
